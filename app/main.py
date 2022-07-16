cf2a0ec145372ea565fe21f85005f553d824d2d4
tomo0037 <b1916984@planet.kanazawa-it.ac.jp>
2022-07-16 09:11:48 +0900

    add

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from flask import Flask,render_template
 
 #Flaskオブジェクトの生成
 #app = Flask(__name__, static_folder='./static/', template_folder='./templates/img') 
-app = Flask(__name__, static_folder='./templates/img/')   
+# app = Flask(__name__, static_folder='./templates/img/')    
+app = Flask(__name__, static_folder='./static')
 
-
-#「/index」へアクセスがあった場合に、「index.html」を返す
+#「/」へアクセスがあった場合に、「index.html」を返す
 @app.route("/")
 def index():
   return render_template("index.html")
