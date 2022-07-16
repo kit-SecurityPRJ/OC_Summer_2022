f0d6398ba9681b2922ebc142d4be9e0aed7f0622
tomo0037 <b1916984@planet.kanazawa-it.ac.jp>
2022-07-17 02:42:54 +0900

    fix

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from flask import Flask,render_template
 
-#Flaskオブジェクトの生成
-#app = Flask(__name__, static_folder='./static/', template_folder='./templates/img') 
-# app = Flask(__name__, static_folder='./templates/img/')    
 app = Flask(__name__, static_folder='./static')
 
-#「/」へアクセスがあった場合に、「index.html」を返す
 @app.route("/")
 def index():
   return render_template("index.html")
